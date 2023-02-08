66bee87b8ec4afdceab467b900c6b612d9d2352c
Gatch-you <yutohagaeng@gmail.com>
2023-02-08 21:05:42 +0900

    Make Editor

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,4 @@
+import { generateKey } from 'crypto';
 import * as React from 'react';
 import styled from 'styled-components';
 
@@ -10,15 +11,23 @@ const StyledButton = styled.button`
     height: 2rem;
     min-width: 5rem;
     padding: 0 1rem;
+    
+
+    &.cancel {
+        background: lightgray;
+        border: 1px sokid generateKey;
+        color:gray;
+    }
     `
 
 interface Props {
+    cancel?: boolean
     children: string
     onClick: () => void
 }
 
 export const Button: React.FC<Props> = (props) => (
-    <StyledButton onClick = {props.onClick}>
+    <StyledButton onClick = {props.onClick} className = {props.cancel ? 'cancel': ''}>
      {props.children}
     </StyledButton>
 )
